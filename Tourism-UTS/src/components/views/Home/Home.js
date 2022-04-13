70cb7cacd11d273772eb00a9d5b559dd88fb7fc5
userthalib <thalibirfan29@gmail.com>
2022-04-13 20:14:38 +0700

    update home

--- Tourism-UTS/src/components/views/Home/Home.js
+++ Tourism-UTS/src/components/views/Home/Home.js
@@ -8,12 +8,12 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 
 const Home = () => (
   <Section variant='has-hero'>
-    <Hero titleText='Let&apos;s explore.' imageSrc='http://uploads.kodilla.com/bootcamp/fer/13.router/image-1.jpg' />
+    <Hero titleText='Segera Kunjungi' imageSrc='https://wallpaperaccess.com/full/441134.jpg' />
     <Grid>
       <Row middle="md">
         <Col md={6}>
-          <h1 className={styles.title}>Open up to the world.</h1>
-          <p className={styles.intro}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultricies eros eu nisl vestibulum iaculis. Maecenas dapibus lorem quis ex ornare dignissim. Donec lobortis commodo augue, vel accumsan nunc facilisis vitae. Suspendisse bibendum congue sapien ut elementum. Nunc pellentesque, tortor et dignissim fermentum, purus mi sagittis neque, ut iaculis arcu lorem nec tellus. Integer in viverra lectus, vitae sollicitudin nulla. Sed mollis at odio eu pretium. Donec porta ligula sed diam pulvinar viverra.</p>
+          <h1 className={styles.title}>Wonderful Indonesia.</h1>
+          <p className={styles.intro}>Temukan berbagai macam destinasi wisata yang tidak pernah anda lihat sebelumnya #WonderfulIndonesia.</p>
         </Col>
         <Col xs={12} md={5} mdOffset={1} xl={3} xlOffset={2}>
           <List variant='solid'>
