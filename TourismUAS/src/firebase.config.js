475792b5b0daf01af7693e4a1f2df0fe3be428da
mashhisyam <haryoh48@gmail.com>
2022-05-17 16:53:11 +0700

    Firebase Configuration

---
+++ TourismUAS/src/firebase.config.js
@@ -0,0 +1,10 @@
+
+export default {
+  apiKey: 'AIzaSyDVRZhUoA6UChTRDg4A-cWOgdMeLr8hnt8',
+  authDomain: 'react-tourism-ad0d3.firebaseapp.com',
+  projectId: 'react-tourism-ad0d3',
+  storageBucket: 'react-tourism-ad0d3.appspot.com',
+  messagingSenderId: '1083904038222',
+  appId: '1:1083904038222:web:71389cf13fded6ca607a1f',
+};
+
